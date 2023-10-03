84a73232446149ffba93e8a0e691c15b21487739
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2023-10-03 22:58:02 +0200

    Checking for Bitrise API updates (#6)
    * Checking for Bitrise API updates
    
    * chores

--- check-bitrise-api.sh
+++ check-bitrise-api.sh
@@ -5,17 +5,26 @@ set -e
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${dir%/*}"
 
-readonly cyan="\033[1;36m"
-readonly normal="\033[0m"
+readonly bitrise_swagger="https://api-docs.bitrise.io/docs/swagger.json"
+readonly swagger_file="bitrise-swagger.json"
+readonly bumpsh_token="$1"
 
 echo
 echo "Checking for changes on Bitrise API"
-echo
 
-echo "."
-echo "."
-echo "."
-echo "."
+echo
+echo "‣ Downloading Swagger file"
+
+rm -rf "$swagger_file"
+curl -fsSL -o "$swagger_file" -C - "$bitrise_swagger"
+
+echo "‣ Deploying new version (if changes)"
+
+if ! which bump-cli >/dev/null; then
+    npm install -g bump-cli >/dev/null 2>&1
+fi
+
+bump deploy "$bitrise_swagger" --doc "bitrise-companion" --token "$bumpsh_token" >/dev/null 2>&1
 
 echo
 echo "Done!"
