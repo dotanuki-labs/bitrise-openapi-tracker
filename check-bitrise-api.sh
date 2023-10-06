97f3aab352523a3bd999dde709ef1cf264ae2cf2
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2023-10-06 23:36:18 +0200

    Prepares open-source release (#8)

--- check-bitrise-api.sh
+++ check-bitrise-api.sh
@@ -18,13 +18,15 @@ echo "‣ Downloading Swagger file"
 rm -rf "$swagger_file"
 curl -fsSL -o "$swagger_file" -C - "$bitrise_swagger"
 
-echo "‣ Deploying new version (if changes)"
+echo "‣ Deploying new version (if changed)"
 
 if ! which bump-cli >/dev/null; then
     npm install -g bump-cli >/dev/null 2>&1
 fi
 
-bump deploy "$bitrise_swagger" --doc "bitrise-companion" --token "$bumpsh_token" >/dev/null 2>&1
+bump deploy "$bitrise_swagger" \
+    --doc "bitrise-openapi-tracker" \
+    --token "$bumpsh_token" >/dev/null 2>&1
 
 echo
 echo "Done!"
